04df1b4a6ba1e0cec8276f40bc83a5314fa8c285
Mattias Andrée <maandree@operamail.com>
2014-05-12 21:47:16 +0200

    Rewrite gamma-randr to use gamma-common
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- src/gamma-randr.h
+++ src/gamma-randr.h
@@ -15,10 +15,13 @@
    along with Redshift.  If not, see <http://www.gnu.org/licenses/>.
 
    Copyright (c) 2010  Jon Lund Steffensen <jonlst@gmail.com>
+   Copyright (c) 2014  Mattias Andrée <maandree@member.fsf.org>
 */
 
 #ifndef REDSHIFT_GAMMA_RANDR_H
 #define REDSHIFT_GAMMA_RANDR_H
+
+#include "gamma-common.h"
 
 #include <stdio.h>
 #include <stdint.h>
@@ -26,36 +29,17 @@
 #include <xcb/xcb.h>
 #include <xcb/randr.h>
 
-#include "redshift.h"
-
 
 typedef struct {
-	xcb_randr_crtc_t crtc;
-	unsigned int ramp_size;
-	uint16_t *saved_ramps;
-} randr_crtc_state_t;
-
-typedef struct {
-	xcb_connection_t *conn;
-	xcb_screen_t *screen;
-	int preferred_screen;
-	int screen_num;
-	int crtc_num;
-	unsigned int crtc_count;
-	randr_crtc_state_t *crtcs;
-} randr_state_t;
+	xcb_screen_t screen;
+	xcb_randr_crtc_t *crtcs;
+} randr_screen_data_t;
 
 
-int randr_init(randr_state_t *state);
-int randr_start(randr_state_t *state);
-void randr_free(randr_state_t *state);
+int randr_init(gamma_server_state_t *state);
+int randr_start(gamma_server_state_t *state);
 
 void randr_print_help(FILE *f);
-int randr_set_option(randr_state_t *state, const char *key, const char *value);
-
-void randr_restore(randr_state_t *state);
-int randr_set_temperature(randr_state_t *state, int temp, float brightness,
-			  const float gamma[3]);
 
 
 #endif /* ! REDSHIFT_GAMMA_RANDR_H */
