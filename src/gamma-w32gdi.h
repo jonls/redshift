50c66f8ae6e96eb1c6d01fc32a90f58d00d1a9ae
Mattias Andrée <maandree@operamail.com>
2014-05-12 20:41:27 +0200

    Rewrite gamma-w32gdi to use gamma-common
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- src/gamma-w32gdi.h
+++ src/gamma-w32gdi.h
@@ -15,10 +15,13 @@
    along with Redshift.  If not, see <http://www.gnu.org/licenses/>.
 
    Copyright (c) 2010  Jon Lund Steffensen <jonlst@gmail.com>
+   Copyright (c) 2014  Mattias Andrée <maandree@member.fsf.org>
 */
 
 #ifndef REDSHIFT_GAMMA_W32GDI_H
 #define REDSHIFT_GAMMA_W32GDI_H
+
+#include "gamma-common.h"
 
 #ifdef HAVE_CONFIG_H
 # include "config.h"
@@ -32,22 +35,10 @@
 #endif
 
 
-typedef struct {
-	WORD *saved_ramps;
-} w32gdi_state_t;
-
-
-int w32gdi_init(w32gdi_state_t *state);
-int w32gdi_start(w32gdi_state_t *state);
-void w32gdi_free(w32gdi_state_t *state);
+int w32gdi_init(gamma_server_state_t *state);
+int w32gdi_start(gamma_server_state_t *state);
 
 void w32gdi_print_help(FILE *f);
-int w32gdi_set_option(w32gdi_state_t *state, const char *key,
-		      const char *value);
-
-void w32gdi_restore(w32gdi_state_t *state);
-int w32gdi_set_temperature(w32gdi_state_t *state, int temp, float brightness,
-			   const float gamma[3]);
 
 
 #endif /* ! REDSHIFT_GAMMA_W32GDI_H */
