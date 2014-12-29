d03960a46ead81ef1f09b32b4659057b5d03a5dc
Jon Lund Steffensen <jonlst@gmail.com>
2014-12-28 23:49:36 -0500

    quartz: Add preserve option to Quartz method
    This change adds the preserve option to the quartz method which makes it
    base the temperature adjustments on the existing gamma ramps when set. This
    requires that the existing gamma ramps are saved at start-up which this
    commit also adds.
    
    One drawback is that the list of displays is enumerated at start-up now which
    means that Redshift will likely not affect displays that are connected after
    start-up. This could be fixed in the future by listening to reconfiguration
    events.

--- src/gamma-quartz.h
+++ src/gamma-quartz.h
@@ -20,11 +20,23 @@
 #ifndef REDSHIFT_GAMMA_QUARTZ_H
 #define REDSHIFT_GAMMA_QUARTZ_H
 
+#include <stdint.h>
+
+#include <ApplicationServices/ApplicationServices.h>
+
 #include "redshift.h"
 
 
 typedef struct {
-	int dummy;
+	CGDirectDisplayID display;
+	uint32_t ramp_size;
+	float *saved_ramps;
+} quartz_display_state_t;
+
+typedef struct {
+	quartz_display_state_t *displays;
+	uint32_t display_count;
+	int preserve;
 } quartz_state_t;
 
 
